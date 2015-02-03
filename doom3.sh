3a87967dbedb3d9de332557de67b63f0232e52cf
Ryan P.C. McQuen <ryan.q@linux.com>
2015-02-03 12:59:05 -0800

    fix autoexec url
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- doom3.sh
+++ doom3.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 ## to customize resolution, aspect ratio, et cetera
-# wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/games/doom3/autoexec.cfg -P \
-# ~/.cxoffice/Steam/drive_c/Program\ Files/Steam/steamapps/common/Doom\ 3/base/
+# wget -N https://raw.githubusercontent.com/ryanpcmcquen/crossoverLaunchers/master/CFG-doom3/autoexec.cfg -P ~/.cxoffice/Steam/drive_c/Program\ Files/Steam/steamapps/common/Doom\ 3/base/
 
 cd ~/.cxoffice/Steam/drive_c/Program\ Files/Steam/steamapps/common/Doom\ 3/
 
