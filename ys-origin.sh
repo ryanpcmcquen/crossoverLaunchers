31bfbbe47cee709a51bf722c2d8bdb02fe8797f0
Ryan P.C. McQuen <ryan.q@linux.com>
2015-02-03 16:20:24 -0800

    adjust ys origin bottle
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- ys-origin.sh
+++ ys-origin.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-cd ~/.cxoffice/Steam/drive_c/Program\ Files/Steam/steamapps/common/Ys\ Origin/
+##cd ~/.cxoffice/Steam/drive_c/Program\ Files/Steam/steamapps/common/Ys\ Origin/
+cd ~/.cxoffice/Ys_Origin/drive_c/Program\ Files/Steam/steamapps/common/Ys\ Origin/
 
 if [ -d /opt/cxoffice/ ]; then
-  primusrun /opt/cxoffice/bin/wine --bottle Steam --cx-app yso_win.exe
+  primusrun /opt/cxoffice/bin/wine --bottle Ys_Origin --cx-app yso_win.exe
 else
-  primusrun ~/cxoffice/bin/wine --bottle Steam --cx-app yso_win.exe
+  primusrun ~/cxoffice/bin/wine --bottle Ys_Origin --cx-app yso_win.exe
 fi
 
