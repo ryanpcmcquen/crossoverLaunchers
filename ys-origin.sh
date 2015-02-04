3337e3f31984be7bcaec62aed61ae63565a10797
Ryan P.C. McQuen <ryan.q@linux.com>
2015-02-03 16:47:13 -0800

    add back some old code (##)
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- ys-origin.sh
+++ ys-origin.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 ##cd ~/.cxoffice/Steam/drive_c/Program\ Files/Steam/steamapps/common/Ys\ Origin/
+##if [ -d /opt/cxoffice/ ]; then
+##  primusrun /opt/cxoffice/bin/wine --bottle Steam --cx-app yso_win.exe
+##else
+##  primusrun ~/cxoffice/bin/wine --bottle Steam --cx-app yso_win.exe
+##fi
+
 cd ~/.cxoffice/Ys_Origin/drive_c/Program\ Files/Steam/steamapps/common/Ys\ Origin/
 
 if [ -d /opt/cxoffice/ ]; then
