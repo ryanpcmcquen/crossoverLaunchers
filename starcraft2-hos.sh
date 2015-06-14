a150a979574ea418d6ba70b248e37a089b76cd69
Ryan P.C. McQuen <ryan.q@linux.com>
2015-06-14 10:50:56 -0700

    fix capitalization for s2
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- starcraft2-hos.sh
+++ starcraft2-hos.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-cd ~/.cxoffice/Starcraft_II_Heart_of_the_Swarm/drive_c/Program\ Files/Starcraft\ II/
+cd ~/.cxoffice/Starcraft_II_Heart_of_the_Swarm/drive_c/Program\ Files/StarCraft\ II/
 
 if [ -d /opt/cxoffice/ ]; then
-  primusrun /opt/cxoffice/bin/wine --bottle Starcraft_II_Heart_of_the_Swarm --cx-app Starcraft\ II.exe
+  primusrun /opt/cxoffice/bin/wine --bottle Starcraft_II_Heart_of_the_Swarm --cx-app StarCraft\ II.exe
 else
-  primusrun ~/cxoffice/bin/wine --bottle Starcraft_II_Heart_of_the_Swarm --cx-app Starcraft\ II.exe
+  primusrun ~/cxoffice/bin/wine --bottle Starcraft_II_Heart_of_the_Swarm --cx-app StarCraft\ II.exe
 fi
 
