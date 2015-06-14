9adc4bf87c4038681f6c625be45011f470e727c1
Ryan P.C. McQuen <ryan.q@linux.com>
2015-06-14 10:52:11 -0700

    more PascalCase fixing
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- starcraft2-hos.sh
+++ starcraft2-hos.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-cd ~/.cxoffice/Starcraft_II_Heart_of_the_Swarm/drive_c/Program\ Files/StarCraft\ II/
+cd ~/.cxoffice/StarCraft_II_Heart_of_the_Swarm/drive_c/Program\ Files/StarCraft\ II/
 
 if [ -d /opt/cxoffice/ ]; then
-  primusrun /opt/cxoffice/bin/wine --bottle Starcraft_II_Heart_of_the_Swarm --cx-app StarCraft\ II.exe
+  primusrun /opt/cxoffice/bin/wine --bottle StarCraft_II_Heart_of_the_Swarm --cx-app StarCraft\ II.exe
 else
-  primusrun ~/cxoffice/bin/wine --bottle Starcraft_II_Heart_of_the_Swarm --cx-app StarCraft\ II.exe
+  primusrun ~/cxoffice/bin/wine --bottle StarCraft_II_Heart_of_the_Swarm --cx-app StarCraft\ II.exe
 fi
 
