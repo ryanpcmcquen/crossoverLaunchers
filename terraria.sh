67fa286f176b17da9a8201677d7d4be452bc6e21
Ryan P.C. McQuen <ryan.q@linux.com>
2016-07-11 06:20:35 -0700

    Remove games that now have Linux support!
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- terraria.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd ~/.cxoffice/Steam/drive_c/Program\ Files/Steam/steamapps/common/Terraria/
-
-if [ -d /opt/cxoffice/ ]; then
-  primusrun /opt/cxoffice/bin/wine --bottle Steam --cx-app Terraria.exe
-else
-  primusrun ~/cxoffice/bin/wine --bottle Steam --cx-app Terraria.exe
-fi
-
--- they-bleed-pixels.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd ~/.cxoffice/Steam/drive_c/Program\ Files/Steam/steamapps/common/they\ bleed\ pixels/
-
-## this one just seems to work better windowed  :-)
-if [ -d /opt/cxoffice/ ]; then
-  primusrun /opt/cxoffice/bin/wine --bottle Steam --cx-app They\ Bleed\ Pixels\ PC.exe -window
-else
-  primusrun ~/cxoffice/bin/wine --bottle Steam --cx-app They\ Bleed\ Pixels\ PC.exe -window
-fi
